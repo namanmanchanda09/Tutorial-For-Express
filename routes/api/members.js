5a0bf5484514ffd158382d34bb74e8b1bc540ab5
namanmanchanda09 <naman.mohan09@outlook.com>
2020-06-08 20:27:32 +0530

    added aatributes

--- routes/api/members.js
+++ routes/api/members.js
@@ -69,6 +69,20 @@ router.put('/:id', (req, res)=>{
     }
 })
 
+router.get('/:id', (req, res)=>{
+    if(req.params.id >3){
+        res.status(400).json(
+                { message : `Member not found with id of ${req.params.id}.` }
+            )  
+    }else{
+
+    
+
+    res.json(members.filter(
+            member => member.id===parseInt(req.params.id)
+        ))
+    }
+})
 
 module.exports = router;
 
