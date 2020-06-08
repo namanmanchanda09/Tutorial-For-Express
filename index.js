5c224aca269664aea9378c3a99841d398be6fc5d
namanmanchanda09 <naman.mohan09@outlook.com>
2020-06-08 15:52:08 +0530

    created NOTES

--- index.js
+++ index.js
@@ -7,6 +7,33 @@ const app = express();
 //     res.sendFile(path.join(__dirname,'public','index.html'));
 // })
 
+const members = [
+    {
+        id:1,
+        name:'Naman',
+        email:'naman.mohan09@outlook.com',
+        status:'active'
+    },
+    {
+        id:2,
+        name:'Tom',
+        email:'tom@gmail.com',
+        status:'inactive'
+    },
+    {
+        id:3,
+        name:'Doug',
+        email:'doug@gmail.com',
+        status:'active'
+    }
+]
+
+// Gets all members
+app.get('/api/members',(req,res)=>{
+    res.json(members);
+
+})
+
 // Set Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -14,6 +41,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT,()=>console.log(`Server started on port ${PORT}`));
+
 
 
 
